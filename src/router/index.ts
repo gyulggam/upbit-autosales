36765ae40a31f67087fcb316b1b042ba6e1abd8d
wdamanghara <67sc2@naver.com>
2021-09-24 01:19:42 +0900

    0904 file arrangement

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,16 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import App from '../App.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: App
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: App
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
